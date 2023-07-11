2bf39d95b0295f41f8d3eca5c080110256159ea3
nyerim <127369495+nyerim@users.noreply.github.com>
2023-07-11 17:24:25 +0900

    문예림 수정

---
+++ index.js
@@ -0,0 +1,30 @@
+/* 마우스 휠 이벤트 */
+$('.event').each(function () {
+  $(this).on('wheel', function (evt) {
+    evt.preventDefault();
+
+    let delta = evt.originalEvent.deltaY;
+    let currentTop = null;
+    const nextBox = this.nextElementSibling;
+    const prevBox = this.previousElementSibling;
+
+    if (delta > 0) {
+      if (nextBox) {
+        currentTop = nextBox.offsetTop;
+      } else {
+        currentTop = this.offsetTop;
+      }
+    } else {
+      if (prevBox) {
+        currentTop = prevBox.offsetTop;
+      } else {
+        return;
+      }
+    }
+
+    scrollTo({
+      top: currentTop,
+      behavior: 'smooth',
+    });
+  });
+});
