718b1fafbe814362ce2474f5d0f8fdc922141d3b
nyerim <yogut3395@naver.com>
2023-07-13 17:48:31 +0900

    .

--- index.js
+++ index.js
@@ -48,3 +48,38 @@ const mySwiper = new Swiper('.swiper-container', {
   delay: 5000,
   },
   });
+
+  /* button */
+
+  $('a[href="#"]').on('click', (evt) => {
+    evt.preventDefault();
+  });
+
+//   function toggleImg1() {
+//     var img1 = document.getElementById("img01");
+//     var img3 = document.getElementById("img03");
+//     img1.src = "./images/a.png";
+//     img3.src = "./images/a.png";
+// }
+
+// function toggleImg2() {
+//     var img2 = document.getElementById("img01");
+//     var img4 = document.getElementById("img03");
+//     img2.src = "./images/city.jpg";
+//     img4.src = "./images/city.jpg";
+// }
+
+function toggleImg1() {
+  var img1 = document.getElementById("img01");
+  var img3 = document.getElementById("img03");
+  img1.src = "./images/a.png";
+  img3.src = "./images/a.png";
+}
+
+function toggleImg2() {
+  var img2 = document.getElementById("img01");
+  var img4 = document.getElementById("img03");
+  img2.src = "./images/city.jpg";
+  img4.src = "./images/city.jpg";
+}
+
