e2a42ce8f6ce491cf4ecbb322545ab86bc8e912d
nyerim <127369495+nyerim@users.noreply.github.com>
2023-07-14 10:40:31 +0900

    예림아 별 움직여라

--- index.js
+++ index.js
@@ -32,28 +32,28 @@ $('.event').each(function () {
 /* swiper */
 const mySwiper = new Swiper('.swiper-container', {
   loop: true,
-  speed : 1000,
+  speed: 1000,
   mousewheel: true,
   coverflowEffect: {
-  rotate: 30,
-  slideShadows: true,
+    rotate: 30,
+    slideShadows: true,
   },
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  
+
   autoplay: {
-  delay: 5000,
+    delay: 5000,
   },
-  });
+});
 
-  /* button */
+/* button */
 
-  $('a[href="#"]').on('click', (evt) => {
-    evt.preventDefault();
-  });
+$('a[href="#"]').on('click', (evt) => {
+  evt.preventDefault();
+});
 
 //   function toggleImg1() {
 //     var img1 = document.getElementById("img01");
@@ -70,21 +70,15 @@ const mySwiper = new Swiper('.swiper-container', {
 // }
 
 function toggleImg1() {
-  var img1 = document.getElementById("img01");
-  var img3 = document.getElementById("img03");
-  img1.src = "./images/city.jpg";
-  img3.src = "./images/city.jpg";
-  btn1.classList.add("active");
-  btn2.classList.remove("active");
+  var img1 = document.getElementById('img01');
+  var img3 = document.getElementById('img03');
+  img1.src = './images/city.jpg';
+  img3.src = './images/city.jpg';
 }
 
 function toggleImg2() {
-  var img2 = document.getElementById("img01");
-  var img4 = document.getElementById("img03");
-  img2.src = "./images/a.png";
-  img4.src = "./images/a.png";
-  btn1.classList.remove("active");
-  btn2.classList.add("active");
+  var img2 = document.getElementById('img01');
+  var img4 = document.getElementById('img03');
+  img2.src = './images/a.png';
+  img4.src = './images/a.png';
 }
-
-
